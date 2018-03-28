2ae8ed540a14f693ca1b97bff945af3504294317
dickmao <rchiang@cs.stonybrook.edu>
2018-03-27 23:43:52 -0400

    must feed CIRCLE_BRANCH env to dedupe

--- ecs-utils.sh
+++ ecs-utils.sh
@@ -69,7 +69,7 @@ EOF
   fi
   SES_USER=${SES_USER:-$(aws configure --profile ses get aws_access_key_id)}
   SES_PASSWORD=${SES_PASSWORD:-$(aws configure --profile ses get aws_secret_access_key)}
-  python render-docker-compose.py $mode --var cluster=$STACK --var GIT_USER=${GIT_USER} --var GIT_PASSWORD=${GIT_PASSWORD} --var AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) --var AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) --var AWS_DEFAULT_REGION=$(aws configure get region) --var MONGO_AUTH_STRING="admin:password@" --var SES_USER=${SES_USER} --var SES_PASSWORD=${SES_PASSWORD}
+  python render-docker-compose.py $mode --var cluster=$STACK --var GIT_USER=${GIT_USER} --var GIT_PASSWORD=${GIT_PASSWORD} --var AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) --var AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) --var AWS_DEFAULT_REGION=$(aws configure get region) --var MONGO_AUTH_STRING="admin:password@" --var SES_USER=${SES_USER} --var SES_PASSWORD=${SES_PASSWORD} --var CIRCLE_BRANCH=${CIRCLE_BRANCH:-}
 }
 
 function getServiceConfigs {
