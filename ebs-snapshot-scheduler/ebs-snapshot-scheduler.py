e2f41e40567224e4feec78959e55125122923b97
dickmao <rchiang@cs.stonybrook.edu>
2018-04-06 15:24:36 -0400

    wasn't snapping anything because incorrect stack_prefix

--- ebs-snapshot-scheduler/ebs-snapshot-scheduler.py
+++ ebs-snapshot-scheduler/ebs-snapshot-scheduler.py
@@ -186,5 +186,5 @@ if __name__ == "__main__":
 
     for instance in boto3.resource('ec2', region_name=args.region).instances.filter(Filters=[{ 'Name': 'tag-key', 'Values': [ custom_tag_name ] }]):
         stackname = next( (t['Value'] for t in instance.tags if t['Key'] == "aws:cloudformation:stack-name"), None)
-        if stackname != None and stackname.startswith(stack_prefix):
+        if stackname != None and stackname.startswith(args.cluster):
             backup_instance(instance, args.region, custom_tag_name, not args.nodry)
