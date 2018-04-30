e3af5f11fdb627541876261696abc52def860d7e
dickmao <rchiang@cs.stonybrook.edu>
2018-04-30 18:05:07 -0400

    I wonder how this ever worked...

--- ecs-down.sh
+++ ecs-down.sh
@@ -80,6 +80,16 @@ for bucket in $(aws cloudformation describe-stack-resources --stack-name $STACK 
   aws s3 rm s3://$bucket/ --recursive || true
 done
 
+if aws cloudformation describe-stacks --stack-name $STACK 2>/dev/null ; then
+    aws cloudformation delete-stack --stack-name $STACK
+    inprog=0
+    while [ $inprog -lt 50 ] && [ "xDELETE_IN_PROGRESS" == "x$(aws cloudformation describe-stacks --stack-name $STACK 2>/dev/null | jq -r ' .Stacks[0] | .StackStatus ')" ]; do
+        echo DELETE_IN_PROGRESS...
+        let inprog=inprog+1
+        sleep 10
+    done
+fi
+
 # https://alestic.com/2016/09/aws-route53-wipe-hosted-zone/
 hosted_zone_id=$(
   aws route53 list-hosted-zones \
@@ -108,16 +118,6 @@ if [ ! -z ${hosted_zone_id} ]; then
   aws route53 delete-hosted-zone \
     --id $hosted_zone_id \
     --output text --query 'ChangeInfo.Id'
-fi
-
-if aws cloudformation describe-stacks --stack-name $STACK 2>/dev/null ; then
-    aws cloudformation delete-stack --stack-name $STACK
-    inprog=0
-    while [ $inprog -lt 50 ] && [ "xDELETE_IN_PROGRESS" == "x$(aws cloudformation describe-stacks --stack-name $STACK 2>/dev/null | jq -r ' .Stacks[0] | .StackStatus ')" ]; do
-        echo DELETE_IN_PROGRESS...
-        let inprog=inprog+1
-        sleep 10
-    done
 fi
 
 clean_state
